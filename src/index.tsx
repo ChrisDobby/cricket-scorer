d02b06ed900c4b056f7bb3a0d789945a95f0f25a
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-04-27 19:55:30 +0100

    Added bootstrap and font awesome

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
     <BrowserRouter>
--- webpack.config.js
+++ webpack.config.js
@@ -24,8 +24,39 @@ module.exports = {
 
     module: {
         rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "ts-loader" },
+            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            },
+            {
+                test: /(\.css)$/,
+                loaders: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.eot(\?v=\d+.\d+.\d+)?$/,
+                loader: 'file-loader',
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader?name=[name].[ext]',
+            },
+            {
+                test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
+                loader: 'file-loader?name=[name].[ext]',
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file-loader?name=[name].[ext]',
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/,
+                use: 'file-loader?name=[name].[ext]',
+            },
+            {
+                test: /\.ico$/,
+                loader: 'file-loader?name=[name].[ext]',
+            },
 
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
