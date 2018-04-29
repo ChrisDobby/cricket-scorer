e98f66f2be5e4358cde07664cfaebf10f9c3a602
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-04-29 11:43:26 +0100

    First pass at domain

---
+++ src/__tests__/domain.tests.ts
@@ -0,0 +1,131 @@
+import * as domain from '../domain';
+
+describe('domain', () => {
+    describe('howOutDescription', () => {
+        it('should return not out for no wicket', () => {
+            const description = domain.howOutDescription(undefined);
+
+            expect(description).toBe('not out');
+        });
+
+        it('should return bowled description for wicket out bowled', () => {
+            const description = domain.howOutDescription({
+                time: new Date(),
+                howOut: domain.Howout.Bowled,
+                bowler: 'A bowler',
+            });
+
+            expect(description).toBe('bowled A bowler');
+        });
+
+        it('should return lbw description for wicket out lbw', () => {
+            const description = domain.howOutDescription({
+                time: new Date(),
+                howOut: domain.Howout.Lbw,
+                bowler: 'A bowler',
+            });
+
+            expect(description).toBe('lbw A bowler');
+        });
+
+        it('should return caught description for wicket out caught', () => {
+            const description = domain.howOutDescription({
+                time: new Date(),
+                howOut: domain.Howout.Caught,
+                bowler: 'A bowler',
+                fielder: 'A fielder',
+            });
+
+            expect(description).toBe('ct A fielder b A bowler');
+        });
+
+        it('should return caught & bowled description for wicket out caught & bowled', () => {
+            const description = domain.howOutDescription({
+                time: new Date(),
+                howOut: domain.Howout.Caught,
+                bowler: 'A bowler',
+                fielder: 'A bowler',
+            });
+
+            expect(description).toBe('ct & bowled A bowler');
+        });
+
+        it('should return run out description for wicket out run out', () => {
+            const description = domain.howOutDescription({
+                time: new Date(),
+                howOut: domain.Howout.RunOut,
+                bowler: 'A bowler',
+            });
+
+            expect(description).toBe('run out');
+        });
+
+        it('should include fielder in run out description if one specified', () => {
+            const description = domain.howOutDescription({
+                time: new Date(),
+                howOut: domain.Howout.RunOut,
+                bowler: 'A bowler',
+                fielder: 'A fielder',
+            });
+
+            expect(description).toBe('run out (A fielder)');
+        });
+
+        it('should return stumped description for wicket out stumped', () => {
+            const description = domain.howOutDescription({
+                time: new Date(),
+                howOut: domain.Howout.Stumped,
+                bowler: 'A bowler',
+                fielder: 'A keeper',
+            });
+
+            expect(description).toBe('st A keeper b A bowler');
+        });
+
+        it('should return retired description for batter retired', () => {
+            const description = domain.howOutDescription({
+                time: new Date(),
+                howOut: domain.Howout.Retired,
+            });
+
+            expect(description).toBe('retired');
+        });
+
+        it('should return timed out description for batter timed out', () => {
+            const description = domain.howOutDescription({
+                time: new Date(),
+                howOut: domain.Howout.TimedOut,
+            });
+
+            expect(description).toBe('timed out');
+        });
+
+        it('should return obstruction description for wicket out obstructing the field', () => {
+            const description = domain.howOutDescription({
+                time: new Date(),
+                howOut: domain.Howout.ObstructingField,
+            });
+
+            expect(description).toBe('obstructing the field');
+        });
+
+        it('should return handled description for wicket out handled the ball', () => {
+            const description = domain.howOutDescription({
+                time: new Date(),
+                howOut: domain.Howout.HandledBall,
+            });
+
+            expect(description).toBe('handled the ball');
+        });
+
+        it('should return hit wicket description for wicket out hit wicket', () => {
+            const description = domain.howOutDescription({
+                time: new Date(),
+                howOut: domain.Howout.HitWicket,
+                bowler: 'A bowler',
+            });
+
+            expect(description).toBe('hit wkt A bowler');
+        });
+    });
+});
---
+++ src/domain.ts
@@ -0,0 +1,129 @@
+export enum Howout {
+    Bowled,
+    Lbw,
+    Caught,
+    RunOut,
+    Stumped,
+    Retired,
+    HitWicket,
+    TimedOut,
+    HandledBall,
+    ObstructingField,
+}
+
+export enum DeliveryOutcome {
+    Dot,
+    Runs,
+    Byes,
+    LegByes,
+    PenaltyRuns,
+    Noball,
+    Wide,
+    Wicket,
+}
+
+export interface Wicket {
+    time: Date;
+    howOut: Howout;
+    bowler?: string;
+    fielder?: string;
+}
+
+export interface Outcome {
+    deliveryOutcome: DeliveryOutcome;
+    score: number;
+}
+
+export interface Delivery {
+    time: Date;
+    bowler: string;
+    batsman: string;
+    outcome: Outcome;
+}
+
+export interface BattingInnings {
+    runs: number;
+    timeIn: Date;
+    deliveries: Delivery[];
+    ballsFaced: number;
+    fours: number;
+    sixes: number;
+    wicket?: Wicket;
+}
+
+export interface Batter {
+    position: number;
+    name: string;
+    innings?: BattingInnings;
+}
+
+export interface Over {
+    overNumber: number;
+    deliveries: Delivery[];
+}
+
+export interface Bowler {
+    position: number;
+    name: string;
+    overs: Over[];
+}
+
+export interface Extras {
+    byes: number;
+    legByes: number;
+    wides: number;
+    noBalls: number;
+    penaltyRuns: number;
+}
+
+export interface Batting {
+    batters: Batter[];
+    extras: Extras;
+}
+
+export interface Innings {
+    battingTeam: string;
+    bowlingTeam: string;
+    score: number;
+    wickets: number;
+    allOut: boolean;
+    balls: number;
+    batting: Batting;
+    bowlers: Bowler[];
+    complete: boolean;
+}
+
+export const howOutDescription = (wicket?: Wicket): string => {
+    const description = (wkt: Wicket): string => {
+        switch (wkt.howOut) {
+        case Howout.Bowled:
+            return `bowled ${wkt.bowler}`;
+        case Howout.Lbw:
+            return `lbw ${wkt.bowler}`;
+        case Howout.Caught:
+            return wkt.bowler === wkt.fielder
+                ? `ct & bowled ${wkt.bowler}`
+                : `ct ${wkt.fielder} b ${wkt.bowler}`;
+        case Howout.RunOut:
+            return wkt.fielder
+                ? `run out (${wkt.fielder})`
+                : 'run out';
+        case Howout.Stumped:
+            return `st ${wkt.fielder} b ${wkt.bowler}`;
+        case Howout.Retired:
+            return 'retired';
+        case Howout.TimedOut:
+            return 'timed out';
+        case Howout.ObstructingField:
+            return 'obstructing the field';
+        case Howout.HandledBall:
+            return 'handled the ball';
+        case Howout.HitWicket:
+            return `hit wkt ${wkt.bowler}`;
+        }
+    };
+
+    return wicket
+        ? description(wicket)
+        : 'not out';
+};
