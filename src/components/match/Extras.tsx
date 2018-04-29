60c198ef57b0377bcbbb9fafbb65bbec550332cd
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-04-29 11:44:28 +0100

    Extras component

---
+++ src/components/match/Extras.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Extras as InningsExtras } from '../../domain';
+import * as styles from './styles';
+
+const extrasLine = ({ noBalls, wides, byes, legByes }: InningsExtras) =>
+    `${noBalls}nb ${wides}wd ${byes}b ${legByes}lb`;
+    
+export interface ExtrasProps { extras: InningsExtras; }
+
+export const Extras = ({ extras }: ExtrasProps) => (
+    <div className="row" style={styles.itemRow}>
+        <div className="col-md-3">Extras</div>
+        <div className="col-md-4">{extrasLine(extras)}</div>
+        <div
+            className="col-md-1"
+            style={styles.numberCell}
+        >{extras.byes + extras.legByes + extras.wides + extras.noBalls}
+        </div>
+    </div>
+);
---
+++ src/components/match/styles.ts
@@ -0,0 +1,21 @@
+
+export const headingRow: React.CSSProperties = {
+    backgroundColor: 'rgb(233, 236, 239)',
+    paddingTop: '4px',
+};
+
+export const itemRow: React.CSSProperties = {
+    marginTop: '8px',
+    borderBottom: '1px solid rgb(233, 236, 239)',
+    fontSize: '14px',
+    paddingBottom: '4px',
+};
+
+export const numberCell: React.CSSProperties = {
+    textAlign: 'right',
+};
+
+export const runsCell: React.CSSProperties = {
+    ...numberCell,
+    fontWeight: 'bold',
+};
