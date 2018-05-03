2320ca65ff904cd5a99b5807e7b412d93539362a
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-05-03 08:14:12 +0100

    Add scorecard route - with test data for now

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
+import WithNavBar from './components/WithNavBar';
+import { Scorecard } from './components/match/Scorecard';
+import { match } from './__tests__/testData/testMatch';
+
+const CardWithNavBar = WithNavBar(Scorecard);
 
 const Routes = () => (
-    <div>
+    <div className="container-fluid">
         <Route exact path="/" render={() => <div id="home-route" />} />
+        <Route exact path="/scorecard" render={props => <CardWithNavBar {...props} cricketMatch={match} />} />
     </div>
 );
 
