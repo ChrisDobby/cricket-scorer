11d5eaf3b0d3de4fa2ef1af1edd6defc782f1204
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-05-20 20:30:44 +0100

    Update inProgressMatchStore tests

--- src/__tests__/stores/InProgressMatchStore.tests.ts
+++ src/__tests__/stores/InProgressMatchStore.tests.ts
@@ -1,5 +1,6 @@
 import inProgressMatchStore from '../../stores/inProgressMatchStore';
 import * as matches from '../testData/matches';
+import { DeliveryOutcome } from '../../domain';
 
 describe('inProgressMatchStore', () => {
     describe('currentInnings', () => {
@@ -112,6 +113,13 @@ describe('inProgressMatchStore', () => {
     });
 
     describe('startInnings', () => {
+        it('should do nothing if no match has been started', () => {
+            inProgressMatchStore.match = undefined;
+            inProgressMatchStore.startInnings(matches.blankMatch.homeTeam, 0, 1);
+
+            expect(inProgressMatchStore.match).toBeUndefined();
+        });
+
         it('should add a new innings to the list of innings in the match', () => {
             inProgressMatchStore.match = matches.blankMatch;
             inProgressMatchStore.startInnings(matches.blankMatch.homeTeam, 0, 1);
@@ -121,11 +129,102 @@ describe('inProgressMatchStore', () => {
     });
 
     describe('newBowler', () => {
+        it('should do nothing if no match has been started', () => {
+            inProgressMatchStore.match = undefined;
+            inProgressMatchStore.newBowler(10);
+
+            expect(inProgressMatchStore.match).toBeUndefined();
+        });
+
+        it('should do nothing if no innings has been started', () => {
+            inProgressMatchStore.match = matches.blankMatch;
+            inProgressMatchStore.newBowler(10);
+
+            expect(inProgressMatchStore.match.innings).toHaveLength(0);
+        });
+
         it('should update the non completed innings with a new bowler', () => {
             inProgressMatchStore.match = matches.matchWithStartedInnings;
             inProgressMatchStore.newBowler(10);
 
             expect(inProgressMatchStore.match.innings[0].bowlers).toHaveLength(1);
+        });
+    });
+
+    describe('dotBall', () => {
+        it('should do nothing if no match has been started', () => {
+            inProgressMatchStore.match = undefined;
+            inProgressMatchStore.dotBall();
+
+            expect(inProgressMatchStore.match).toBeUndefined();
+        });
+
+        it('should do nothing if no innings has been started', () => {
+            inProgressMatchStore.match = matches.blankMatch;
+            inProgressMatchStore.dotBall();
+
+            expect(inProgressMatchStore.match.innings).toHaveLength(0);
+        });
+
+        it('should do nothing if no over has been started', () => {
+            inProgressMatchStore.match = matches.matchWithStartedInnings;
+            inProgressMatchStore.dotBall();
+
+            expect(inProgressMatchStore.match.innings[0].deliveries).toHaveLength(0);
+        });
+
+        it('should add a dot ball to the innings deliveries', () => {
+            inProgressMatchStore.match = matches.matchWithStartedOver;
+            inProgressMatchStore.dotBall();
+
+            expect(inProgressMatchStore.match.innings[0].deliveries).toHaveLength(1);
+            expect(inProgressMatchStore.match.innings[0].deliveries[0].outcome.deliveryOutcome)
+                .toBe(DeliveryOutcome.Dot);
+        });
+    });
+
+    describe('completeOver', () => {
+        it('should do nothing if no match has been started', () => {
+            inProgressMatchStore.match = undefined;
+            inProgressMatchStore.completeOver();
+
+            expect(inProgressMatchStore.match).toBeUndefined();
+        });
+
+        it('should do nothing if no innings has been started', () => {
+            inProgressMatchStore.match = matches.blankMatch;
+            inProgressMatchStore.completeOver();
+
+            expect(inProgressMatchStore.match.innings).toHaveLength(0);
+        });
+
+        it('should do nothing if no over has been started', () => {
+            inProgressMatchStore.match = matches.matchWithStartedInnings;
+            inProgressMatchStore.completeOver();
+
+            expect(inProgressMatchStore.match.innings[0].completedOvers).toBe(0);
+        });
+
+        it('should update the current over count', () => {
+            inProgressMatchStore.match = matches.matchWithOverReadyToComplete;
+            inProgressMatchStore.completeOver();
+
+            expect(inProgressMatchStore.match.innings[0].completedOvers).toBe(1);
+        });
+
+        it('should swap the current batter over', () => {
+            inProgressMatchStore.match = matches.matchWithOverReadyToComplete;
+            inProgressMatchStore.completeOver();
+
+            expect(inProgressMatchStore.currentBatter)
+                .toBe(inProgressMatchStore.match.innings[0].batting.batters[1]);
+        });
+
+        it('should remove the current bowler', () => {
+            inProgressMatchStore.match = matches.matchWithOverReadyToComplete;
+            inProgressMatchStore.completeOver();
+
+            expect(inProgressMatchStore.currentBowler).toBeUndefined();
         });
     });
 });
