04deeaf0aeb1700eefd479082e1419092badc2ce
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-05-29 10:20:35 +0100

    Add blank to scores entry when no dot available

--- src/components/match/ScoresEntry.tsx
+++ src/components/match/ScoresEntry.tsx
@@ -3,6 +3,11 @@ import { DeliveryOutcome, DeliveryScores } from '../../domain';
 import { ActionButton } from './ActionButton';
 import { OtherScore } from './OtherScore';
 
+const spacerStyle: React.CSSProperties = {
+    width: '40px',
+    display: 'inline-block',
+};
+
 export interface ScoresEntryProps {
     showDot: boolean;
     deliveryOutcome: DeliveryOutcome;
@@ -16,6 +21,8 @@ export const ScoresEntry = ({ showDot, deliveryOutcome, getScores, action }: Sco
         <div className="col-10">
             {showDot &&
                 <ActionButton caption="." action={() => action(deliveryOutcome, getScores(0))} noBall={noBall} />}
+            {!showDot &&
+                <span style={spacerStyle}/>}
             <ActionButton caption="1" action={() => action(deliveryOutcome, getScores(1))} noBall={noBall} />
             <ActionButton caption="2" action={() => action(deliveryOutcome, getScores(2))} noBall={noBall} />
             <ActionButton caption="3" action={() => action(deliveryOutcome, getScores(3))} noBall={noBall} />
