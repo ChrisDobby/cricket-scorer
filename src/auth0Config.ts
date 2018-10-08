cfbf6150db0ba2e4c2efb2a2bf2d0f365e117979
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-10-08 11:43:14 +0100

    Test settings

--- src/auth0Config.ts
+++ src/auth0Config.ts
@@ -1,2 +1,2 @@
-export const domain = process.env.AUTH0_DOMAIN;
-export const clientId = process.env.AUTH0_CLIENT_ID;
+export const domain = process.env.AUTH0_DOMAIN || '';
+export const clientId = process.env.AUTH0_CLIENT_ID || '';
