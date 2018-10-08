769b4261b1af25d50fea41f99cc9acdf4ce9685d
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-10-08 14:37:12 +0100

    Put the redirect in the correct place to pick up all requests

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const path = require('path');
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.static('dist'));
-
 app.get('*', (req, res, next) => {
     if (req.headers['x-forwarded-proto'] !== 'https') {
-        res.redirect(`https://${req.hostname}/${req.url}`);
+        res.redirect(`https://${req.hostname}${req.url}`);
     } else {
         next();
     }
 });
+
+app.use(express.static('dist'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './dist/index.html'));
