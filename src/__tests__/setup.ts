e7589f4269f877dd1c189247104d2d5108595c78
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-10-08 11:33:33 +0100

    Add mock values for the auth0 env variables

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -2,3 +2,6 @@ import { configure } from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+process.env.AUTH0_DOMAIN = 'domain';
+process.env.AUTH0_CLIENT_ID = 'clientid';
