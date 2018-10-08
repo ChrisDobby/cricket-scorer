d84016994108606028c02a71fdfda0c15e93d71e
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-10-08 08:57:29 +0100

    Attempt at getting deploy to heroku working!

---
+++ app.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static('dist'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './dist/index.html'));
+});
+
+app.listen(port, (err) => {
+    if (err) { console.log(err); }
+});
