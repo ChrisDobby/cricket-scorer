96a7613cb83f8616ce09243b438dc0ca084ddb5e
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-10-14 20:23:52 +0100

    Set the date when creating a new match

--- src/match/create.ts
+++ src/match/create.ts
@@ -14,28 +14,26 @@ type MatchData = {
     awayPlayers: string[],
 };
 
-export default (matchData: MatchData): Match => {
-    return {
-        user: matchData.username,
-        config: {
-            playersPerSide: matchData.playersPerSide,
-            type: matchData.matchType,
-            inningsPerSide: matchData.matchType === MatchType.LimitedOvers ? 1 : matchData.inningsPerSide,
-            oversPerSide: matchData.matchType === MatchType.LimitedOvers ? matchData.oversPerSide : undefined,
-            runsForNoBall: matchData.runsPerNoBall,
-            runsForWide: matchData.runsPerWide,
-        },
-        homeTeam: {
-            name: matchData.homeTeam,
-            players: [...matchData.homePlayers],
-        },
-        awayTeam: {
-            name: matchData.awayTeam,
-            players: [...matchData.awayPlayers],
-        },
-        date: '',
-        complete: false,
-        status: '',
-        innings: [],
-    };
-};
+export default (matchData: MatchData): Match => ({
+    user: matchData.username,
+    config: {
+        playersPerSide: matchData.playersPerSide,
+        type: matchData.matchType,
+        inningsPerSide: matchData.matchType === MatchType.LimitedOvers ? 1 : matchData.inningsPerSide,
+        oversPerSide: matchData.matchType === MatchType.LimitedOvers ? matchData.oversPerSide : undefined,
+        runsForNoBall: matchData.runsPerNoBall,
+        runsForWide: matchData.runsPerWide,
+    },
+    homeTeam: {
+        name: matchData.homeTeam,
+        players: [...matchData.homePlayers],
+    },
+    awayTeam: {
+        name: matchData.awayTeam,
+        players: [...matchData.awayPlayers],
+    },
+    date: (new Date()).toISOString(),
+    complete: false,
+    status: '',
+    innings: [],
+});
