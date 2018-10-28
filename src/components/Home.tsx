46a8aae7a423185af32124abfc7f26004be60605
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-10-28 20:25:25 +0000

    Added icon license message

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -149,6 +149,28 @@ export default withStyles(styles)(class extends React.PureComponent<any> {
                         </Card>
                     </Grid>
                 </Grid>
+                <footer style={{ bottom: 0 }}>
+                    <Typography variant="subtitle2" color="inherit">
+                        {'Icons made by '}
+                        <a
+                            href="http://www.freepik.com"
+                            title="Freepik"
+                        >Freepik
+                        </a>{' from '}
+                        <a
+                            href="https://www.flaticon.com/"
+                            title="Flaticon"
+                        >
+                            www.flaticon.com
+                        </a>{' is licensed by '}
+                        <a
+                            href="http://creativecommons.org/licenses/by/3.0/"
+                            title="Creative Commons BY 3.0"
+                            target="_blank"
+                        >CC 3.0 BY
+                        </a>
+                    </Typography>
+                </footer>
             </div>);
     }
 });
