b7a8f96550d91a4164d04ed9121604c4ab97592a
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-11-18 10:33:57 +0000

    Function to rollback to an event in the innings

---
+++ src/__tests__/match/innings/rollback.tests.ts
@@ -0,0 +1,72 @@
+import rollback from '../../../match/innings/rollback';
+import * as matches from '../../testData/matches';
+import * as domain from '../../../domain';
+
+describe('rollback', () => {
+    beforeEach(() => jest.clearAllMocks());
+
+    const rebuild = jest.fn(() => ({
+        innings: matches.inningsAfterWicketTaken,
+        batterIndex: 999,
+    }));
+
+    const Rollback = rollback(rebuild);
+
+    const withEvents = {
+        ...matches.startedInnings,
+        events: [
+            {
+                time: (new Date()).getTime(),
+                type: domain.EventType.Delivery,
+                bowlerIndex: 0,
+                batsmanIndex: 0,
+                overNumber: 1,
+                outcome: { scores: { runs: 2 }, deliveryOutcome: domain.DeliveryOutcome.Valid },
+            },
+            {
+                time: (new Date()).getTime(),
+                type: domain.EventType.Delivery,
+                bowlerIndex: 0,
+                batsmanIndex: 0,
+                overNumber: 1,
+                outcome: { scores: {}, deliveryOutcome: domain.DeliveryOutcome.Valid },
+            },
+        ],
+    };
+
+    it('should do nothing if the innings has no events', () => {
+        const rolledBack = Rollback(matches.startedInnings, 2);
+
+        expect(rebuild).not.toHaveBeenCalled();
+        expect(rolledBack).toEqual([
+            matches.startedInnings,
+            0,
+            0,
+        ]);
+    });
+
+    it('should do nothing if the index to roll back to does not exist in the events', () => {
+        const rolledBack = Rollback(withEvents, 2);
+
+        expect(rebuild).not.toHaveBeenCalled();
+        expect(rolledBack).toEqual([
+            withEvents,
+            0,
+            0,
+        ]);
+    });
+
+    it('should rebuild the innings up to the specified event index', () => {
+        const rolledBack = Rollback(withEvents, 0);
+
+        expect(rebuild).toHaveBeenCalledWith(
+            withEvents,
+            0,
+            [withEvents.events[0]],
+        );
+        expect(rolledBack).toEqual([
+            matches.inningsAfterWicketTaken,
+            999,
+            withEvents.events[0].bowlerIndex]);
+    });
+});
---
+++ src/match/innings/rollback.ts
@@ -0,0 +1,18 @@
+import * as domain from '../../domain';
+
+export default (
+    rebuild: (innings: domain.Innings, batterIndex: number, events: domain.Event[]) => domain.RebuiltInnings,
+) =>
+    (innings: domain.Innings, eventIndex: number):
+        [domain.Innings, number, number] => {
+        if (innings.events.length === 0 || eventIndex > innings.events.length - 1) {
+            return [innings, 0, 0];
+        }
+
+        const events = innings.events.filter((ev, idx) => idx <= eventIndex);
+        const deliveries = events.filter(ev => ev.type === domain.EventType.Delivery).map(ev => ev as domain.Delivery);
+        const lastDelivery = deliveries[deliveries.length - 1];
+        const rebuilt = rebuild(innings, lastDelivery.batsmanIndex, events);
+
+        return [rebuilt.innings, rebuilt.batterIndex, lastDelivery.bowlerIndex];
+    };
