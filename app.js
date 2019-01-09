53de0f9bc506f5abc32b4aeba9947f8085977038
Chris Dobson <dobby@chrisjdobson.co.uk>
2019-01-09 12:17:19 +0000

    Enable compression on the app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -15,6 +16,7 @@ app.get('*', (req, res, next) => {
     }
 });
 
+app.use(compression());
 app.use(express.static('dist'));
 
 app.get('*', (req, res) => {
