6d2894f347b22dced1471e93009e106bc5d738b3
Chris Dobson <dobby@chrisjdobson.co.uk>
2019-01-30 10:51:44 +0000

    Get rid of the loading spinner if offline and not loading

--- src/components/useInProgressMatches.ts
+++ src/components/useInProgressMatches.ts
@@ -155,6 +155,7 @@ export default (
         if (status === ONLINE) {
             getMatches();
         } else {
+            setLoadingMatches(false);
             setInProgressMatches(sortedMatches(addExtraMatches([], true)));
             setRetry();
         }
