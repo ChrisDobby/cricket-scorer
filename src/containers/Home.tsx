2c101522c174d1c4108f31770b7b509bea37b75c
Chris Dobson <dobby@chrisjdobson.co.uk>
2019-01-30 16:50:14 +0000

    Fixed continue scoring option on the home page

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -3,8 +3,9 @@ import { default as HomeComponent } from '../components/Home';
 import matchStorage from '../stores/matchStorage';
 import fetchMatch from '../match/fetchMatch';
 import PageContext from '../context/PageContext';
+import WithMatchApi from '../components/WithMatchApi';
 
-export default (props: any) => {
+export default WithMatchApi((props: any) => {
     const { setOptions } = React.useContext(PageContext);
     React.useEffect(setOptions, []);
 
@@ -15,4 +16,4 @@ export default (props: any) => {
             fetchMatch={fetchMatch(props.matchApi, matchStorage(localStorage))}
         />
     );
-};
+});
