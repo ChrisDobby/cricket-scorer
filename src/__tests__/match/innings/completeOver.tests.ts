46d8570962d9857a74c48a70ac4abb1ba60456ad
Chris Dobson <dobby@chrisjdobson.co.uk>
2019-02-06 14:06:22 +0000

    Do not add completed over, maiden if bowler didn't bowl the full over

--- src/__tests__/match/innings/completeOver.tests.ts
+++ src/__tests__/match/innings/completeOver.tests.ts
@@ -47,4 +47,25 @@ describe('completeOver', () => {
         expect((<OverComplete>innings.events[6]).batsmanIndex).toBe(0);
         expect((<OverComplete>innings.events[6]).bowlerIndex).toBe(0);
     });
+
+    it('should only update the bowlers completed overs if the bowler bowled all of the over', () => {
+        const inningsWithOverBowledByDifferentBowlers = {
+            ...matches.inningsWithOverReadyToComplete,
+            events: matches.inningsWithOverReadyToComplete.events.map((ev, index) =>
+                index < 3 ? ev : { ...ev, bowlerIndex: 1 },
+            ),
+        };
+
+        const [innings] = completeOver(
+            inningsWithOverBowledByDifferentBowlers,
+            1,
+            inningsWithOverBowledByDifferentBowlers.batting.batters[0],
+            inningsWithOverBowledByDifferentBowlers.bowlers[0],
+        );
+
+        expect(innings.bowlers[0].completedOvers).toBe(0);
+        expect(innings.bowlers[0].maidenOvers).toBe(0);
+        expect(innings.bowlers[1].completedOvers).toBe(0);
+        expect(innings.bowlers[1].maidenOvers).toBe(0);
+    });
 });
--- src/match/innings/completeOver.ts
+++ src/match/innings/completeOver.ts
@@ -3,20 +3,24 @@ import flipBatters from './flipBatters';
 import { latestOver, isMaidenOver } from '../utilities';
 
 export default (innings: Innings, time: number, batter: Batter, bowler: Bowler): [Innings, number] => {
+    const bowlerIndex = innings.bowlers.indexOf(bowler);
     const event = {
         time,
+        bowlerIndex,
         type: EventType.OverComplete,
         batsmanIndex: innings.batting.batters.indexOf(batter),
-        bowlerIndex: innings.bowlers.indexOf(bowler),
     } as Event;
 
     const over = latestOver(innings.events, innings.completedOvers);
-    const updatedBowler = {
-        ...bowler,
-        completedOvers: bowler.completedOvers + 1,
-        totalOvers: oversDescription(bowler.completedOvers + 1, []),
-        maidenOvers: isMaidenOver(over) ? bowler.maidenOvers + 1 : bowler.maidenOvers,
-    };
+    const bowlerBowledFullOver = !over.find(o => o.bowlerIndex !== bowlerIndex);
+    const updatedBowler = bowlerBowledFullOver
+        ? {
+              ...bowler,
+              completedOvers: bowler.completedOvers + 1,
+              totalOvers: oversDescription(bowler.completedOvers + 1, []),
+              maidenOvers: isMaidenOver(over) ? bowler.maidenOvers + 1 : bowler.maidenOvers,
+          }
+        : bowler;
 
     const nextBatterIndex = flipBatters(innings, batter);
 
