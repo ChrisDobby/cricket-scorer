2ab7684c0769f60d60ebcf17b6cffb3bd89390ff
Chris Dobson <dobby@chrisjdobson.co.uk>
2019-02-07 11:20:19 +0000

    Removed old commented out text

--- src/components/aboutText.ts
+++ src/components/aboutText.ts
@@ -1,8 +1,1 @@
-// export default 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit non velit id suscipit. ' +
-//     'In eget mollis nibh. ' +
-//     'Duis nisi ligula, vestibulum non lorem quis, mattis ultrices nulla. Curabitur mollis porta justo eu ornare. ' +
-//     'Nunc malesuada placerat nisl vitae ultricies. Nulla facilisi. Nam scelerisque euismod nisi.' +
-//     'Donec bibendum sem est, vitae pellentesque massa varius vel. Sed volutpat lectus nec lectus ornare luctus. ' +
-//     'Nunc vestibulum auctor augue, vitae venenatis orci consequat nec.';
-
 export default 'Cricket Scores Live is a free online cricket scoring application.  Users are able to create matches and score them, the scores and scorecards will be updated in real time in the match centre for anyone to view.';
