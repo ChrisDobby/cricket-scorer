e534e4b0a9a11afef9777a7886d62f62596591ec
Chris Dobson <dobby@chrisjdobson.co.uk>
2019-02-07 15:41:32 +0000

    Include boundaries when checking for a maiden over

--- src/match/utilities.ts
+++ src/match/utilities.ts
@@ -7,7 +7,8 @@ export const isMaidenOver = (deliveries: domain.Delivery[]) =>
     deliveries.filter(
         delivery =>
             delivery.outcome.deliveryOutcome === domain.DeliveryOutcome.Valid &&
-            (typeof delivery.outcome.scores.runs === 'undefined' || delivery.outcome.scores.runs === 0),
+            (typeof delivery.outcome.scores.runs === 'undefined' || delivery.outcome.scores.runs === 0) &&
+            typeof delivery.outcome.scores.boundaries === 'undefined',
     ).length === deliveries.length;
 
 export const getTeam = (match: domain.Match, teamType: domain.TeamType) =>
