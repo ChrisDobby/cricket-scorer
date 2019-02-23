3fdd5e5beffbed6f0db3bfe89ca1e5ea387c9bda
ChrisDobby <dobby@chrisjdobson.co.uk>
2019-02-23 08:15:55 +0000

    Change tooltip display

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -8,7 +8,7 @@ interface TooltipProps {
 }
 
 export default (props: TooltipProps) => (
-    <Tooltip disableFocusListener disableTouchListener TransitionComponent={Zoom} title={props.title}>
+    <Tooltip TransitionComponent={Zoom} title={props.title}>
         {props.children}
     </Tooltip>
 );
