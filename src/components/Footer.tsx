c90ba407bbaf4b2b409909f63fa35a88d42b9029
ChrisDobby <dobby@chrisjdobson.co.uk>
2019-06-07 20:20:41 +0100

    Added aria labels to the links on the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -45,12 +45,19 @@ const Footer = (props: any) => (
                         className="fab fa-twitter"
                         target="_blank"
                         rel="noopener noreferrer"
+                        aria-label="Go to ChrisDobby on Twitter"
                     >
                         <span style={{ display: 'none' }}>Twitter</span>
                     </a>
                 </li>
                 <li style={itemStyle}>
-                    <a href="https://chrisdobby.dev" style={linkStyle} target="_blank" rel="noopener noreferrer">
+                    <a
+                        href="https://chrisdobby.dev"
+                        style={linkStyle}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="Go to chrisdobby.dev"
+                    >
                         <span style={{ display: 'none' }}>Website</span>
                         <i className="fas fa-globe" />
                     </a>
@@ -62,6 +69,7 @@ const Footer = (props: any) => (
                         className="fab fa-github"
                         target="_blank"
                         rel="noopener noreferrer"
+                        aria-label="Go to the cricket scorer github page"
                     >
                         <span style={{ display: 'none' }}>Github</span>
                     </a>
